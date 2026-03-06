ec51e91d846dc38805792a30235845fcc5fc12cc
ramil <rm.tj.777@gmail.com>
2026-03-06 13:42:23 +0300

    init

---
+++ src/main/java/blps/itmo/BlpsApplication.java
@@ -0,0 +1,12 @@
+package blps.itmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlpsApplication.class, args);
+    }
+}
---
+++ src/test/java/blps/itmo/BlpsApplicationTests.java
@@ -0,0 +1,13 @@
+package blps.itmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
